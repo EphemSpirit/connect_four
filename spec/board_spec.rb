6c453c81f206c11d0f6babbc22c9ada83559cf8b
Drew Hund <languageinvestigator@gmail.com>
2020-05-13 19:24:31 -0400

    added tests for 2 win conditions

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -19,12 +19,12 @@ describe Board do
   describe("#valid_move?") do
 
     it "returns true is space is empty" do
-      double(:choice => 2, :board[5][@choice]=> [" "])
+      double(:choice => 2, :board[5][@choice] => " ")
       expect(valid_move?(@choice)).to be_valid_move
     end
 
     it "returns false is space taken" do
-      double(:choice => 2, :board[5][@choice] => ["R"])
+      double(:choice => 2, :board[5][@choice] => "R")
       expect(valid_move?(@choice)).not_to be_valid_move
     end
 
@@ -33,8 +33,8 @@ describe Board do
   describe("#mark_square") do
 
     it "marks chosen square with player symbol" do
-      double(:choice => 2, :board[5][@choice] => [" "], :player.marker => "R")
-      expect(mark_square(2)).to eq(:board[5][@choice] => ["R"])
+      double(:choice => 2, :board[5][@choice] => " ", :player.marker => "R")
+      expect(mark_square(2)).to eq(:board[5][@choice] => "R")
       end
 
   end
@@ -47,5 +47,26 @@ describe Board do
     end
 
   end
+
+  describe("#find_horizontal") do
+
+    it "returns true if 4 horizontal" do
+      double(:board[2] => ["R", "R", "R", "R", " ", " ", " ", " "])
+      expect(find_horizontal(:board)).to eq true
+    end
+
+  end
+
+  describe("#find_vertical") do
+    
+    it "returns true if 4 vertical" do
+      @board.each do |row|
+        row[2] = "R"
+      end
+      expect(find_vertical(@board)).to eq true
+    end
+
+  end
+
 
 end
